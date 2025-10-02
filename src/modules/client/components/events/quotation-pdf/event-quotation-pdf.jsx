f6ff445e4a1594ebcc8a36eb18340e1b7f1ea1cc
Huayta <jdavidhuay@gmail.com>
2025-10-02 00:29:44 -0500

    primera pagina iamgenes

--- src/modules/client/components/events/quotation-pdf/event-quotation-pdf.jsx
+++ src/modules/client/components/events/quotation-pdf/event-quotation-pdf.jsx
@@ -1,5 +1,8 @@
 import { Document, Page, Text, View, StyleSheet, pdf, Image } from '@react-pdf/renderer';
-import { foto_3,foto_4, foto_5, foto_6 } from '../../../../../assets/images/events';
+import { foto_3,foto_4, foto_5, foto_6, foto_1 } from '../../../../../assets/images/events';
+import { imagen_2,imagen_3, imagen_4, imagen_5, imagen_6 } from '../../../../../assets/images/home';
+import { imagen_1 } from '../../../../../assets/images/carrousel';
+
 
 
 
@@ -28,6 +31,28 @@ const styles = StyleSheet.create({
   textoConditions: { fontSize: 12, marginBottom: 4, paddingTop:4, marginLeft:15   },
   checkIcon: { width: 10, height: 10, marginRight: 6, marginTop:4, color:'black' },
   firma: { width: 200, height: 100, marginTop: 10  },
+});
+const styles2 = StyleSheet.create({
+  page: { padding: 30, position: 'relative' },
+  logo: { width: 80, height: 80, marginBottom: 10 },
+  headerText: { fontSize: 14, fontWeight: 'bold', textAlign: 'right' },
+  galleryTitle: { fontSize: 12, fontWeight: 'bold', textAlign: 'center', marginBottom: 10 },
+ grid: {
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  marginBottom: 20,
+},
+gridItem: {
+  width: 160, // 3 x 160 = 480 + márgenes
+  height: 110,
+  margin: 8,
+},
+  footer: { position: "absolute", bottom: 30, left: 30, right: 30, width: "90%", flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
+  footerText: { fontSize: 10, color: "#888" },
+  contactRow: { flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 10 },
+  contactIcon: { width: 14, height: 14, marginRight: 4 },
+  contactText: { fontSize: 10, color: "#888", marginRight: 20 }
 });
 
 export const handleDownloadPdf = async (quotation) => {
@@ -87,6 +112,26 @@ const MyQuotationPdf = ({ quotation }) => {
   const servicios = quotation.services_requested || [];
   return (
   <Document>
+
+      <Page size="A4" style={styles2.page}>
+      <PdfHeader />
+      <Text style={styles2.galleryTitle}>LEVEL MUSIC CORP SAC – Evento & Música Profesional</Text>
+      <View style={styles2.grid}>
+        <Image src={imagen_2} style={styles2.gridItem} />
+        <Image src={imagen_3} style={styles2.gridItem} />
+        <Image src={imagen_4} style={styles2.gridItem} />
+        <Image src={imagen_5} style={styles2.gridItem} />
+        <Image src={imagen_6} style={styles2.gridItem} />
+        <Image src={imagen_1} style={styles2.gridItem} />
+        <Image src={foto_1} style={styles2.gridItem} />
+      </View>
+     
+      <PdfFooter />
+    </Page>
+
+
+
+
     <Page size="A4" style={styles.page}>
       {/* Imagen de fondo */}
       <Image src={foto_4} style={styles.bgImage} />
